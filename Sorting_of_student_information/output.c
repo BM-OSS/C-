936615da31621a75a7102b23af001e10001b39f7
炼天魔尊 <1961553620@qq.com>
2025-12-13 21:18:59 +0800

    Delete Sorting_of_student_information directory

--- Sorting_of_student_information/output.c
+++
@@ -1,9 +0,0 @@
-#include "student.h"
-
-void student_output_information(Student *students,int count)
-{
-    for(int i;i<count;i++)
-    {
-        printf("Name:%s ID:%d Score:%.1f\n",students[i].Name,students[i].ID,students[i].Score);
-    }
-}
